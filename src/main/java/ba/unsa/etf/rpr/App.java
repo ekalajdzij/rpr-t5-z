16f0a399dff28332507291769d3af2e58f117bcc
Emir <ekalajdzij1@etf.unsa.ba>
2022-12-06 21:03:10 +0100

    Initial commit: prvi i drugi zadatak

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -6,17 +6,14 @@ import ba.unsa.etf.rpr.FactoryPattern.OperativniSistem;
 import ba.unsa.etf.rpr.FactoryPattern.OperativniSistemFactory;
 import ba.unsa.etf.rpr.FactoryPattern.Android;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
         Phone mobitel = (new PhoneBuilder("Android")).setBaterija(5000).setMegapiksel(48).setProcesor("Snapdragon 865 5nm").setDisplaySize(33.3).build();
         System.out.println(mobitel);
+
+        OperativniSistemFactory operativniSistemFactory = new OperativniSistemFactory();
+        OperativniSistem os = operativniSistemFactory.getInstance("Closed source");
+        os.specifikacije();
+
     }
-    OperativniSistemFactory operativniSistemFactory = new OperativniSistemFactory();
-    OperativniSistem os = operativniSistemFactory.getInstance("Closed source");
-    os.specifikacije();
 }
